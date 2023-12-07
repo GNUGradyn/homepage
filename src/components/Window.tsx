b7cf7869fee7255000afb520884544808f609fea
Gradyn Wursten <me@gradyn.com>
2023-12-06 19:02:21 -0700

    fix

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,5 +1,8 @@
 import "./Window.css"
-import {CSSProperties} from "react";
+import {CSSProperties, useEffect, useRef} from "react";
+import useDraggables from "../hooks/useDraggables";
+import {useDraggable} from "@dnd-kit/core";
+import {CSS} from "@dnd-kit/utilities";
 
 interface WindowProps {
     title: string
@@ -10,12 +13,37 @@ interface WindowProps {
 }
 
 const Window: React.FC<WindowProps> = (props: WindowProps) => {
+
+    const {map: coordinatesMap, setMap: setCoordinatesMap} = useDraggables();
+
+    const {attributes, listeners, setNodeRef, transform} = useDraggable({
+        id: props.title + "-win"
+    });
+    const style = {
+        transform: CSS.Translate.toString(transform),
+    };
+
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (coordinatesMap[props.title + "-win"] != undefined) {
+            if (ref.current != undefined) {
+                ref.current.style.position = "absolute";
+                ref.current.style.top = coordinatesMap[props.title + "-win"].y + "px";
+                ref.current.style.left = coordinatesMap[props.title + "-win"].x + "px";
+            }
+        }
+    }, [coordinatesMap]);
+
+
     return (
-        <div style={{...props.style, width: props.width, height: props.height}} className="window" draggable={true}>
-            <div className="window-head">
+        <div style={{...props.style, ...style, width: props.width, height: props.height}} className="window" ref={ref} {...listeners} {...attributes}>
+            <div className="window-head" ref={setNodeRef}>
                 <p>{props.title}</p>
             </div>
-            {props.children}
+            <div className="window-body">
+                {props.children}
+            </div>
         </div>
     );
 };
